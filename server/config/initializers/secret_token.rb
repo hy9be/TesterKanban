2de59573f476f0c3faa62c945392b6aeb2195832
hy9be <hy9be@virginia.edu>
2014-01-08 13:40:55 -0800

    Reorganize the folders to be frontend and backend

--- server/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Server::Application.config.secret_token = '3eb9279bbaf508e7b1252f29839761bc28aababcc5c996010a37f57d7a27cf728cd6da507846bbe0dc7ddeb07f9f42c94d7cbb4a22727d889c9ed2776acaab9a'
