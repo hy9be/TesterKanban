70fbd84637ac746283c46c16502b8a2f78378544
hy9be <hy9be@virginia.edu>
2014-01-08 16:10:07 -0800

    More client side work done. Need to use client side MVC framework such as Ember.js, Angular.js to clean up architecture.

--- fontend/js/constructDataProvider.js
+++ fontend/js/constructDataProvider.js
@@ -1,11 +1,11 @@
 function getPortletHTML(column_id, portlet_name, portlet_detail) {
     return $('<div/>', {class: 'portlet'}).append(
-        $('<div/>', {class: 'portlet-header class' + column_id, text: portlet_name}),
+        $('<div/>', {class: 'portlet-header ' + column_id, text: portlet_name}),
         $('<div/>', {class: 'portlet-content', text: portlet_detail}));
 }
 
 function getColumnHTML(column_id, column_name, portlets_data) {            
-    portletsHTML = $('<div/>', {class: 'column class' + column_id});
+    portletsHTML = $('<div/>', {class: 'column ' + column_id});
     
     $.each(portlets_data, function(i, item) {
         portletsHTML.append(getPortletHTML(column_id, item.portlet_name, item.portlet_detail));
@@ -14,17 +14,17 @@ function getColumnHTML(column_id, column_name, portlets_data) {
     return $('<div/>', {class: 'column-container'}).append(
         // the column header
         $('<div/>', {class: 'column-header'}).append(
-            $('<h2/>', {class: 'header', text: column_name})), portletsHTML);
+            $('<span/>', {class: 'header h2', text: column_name})), portletsHTML);
 }
 
 // When document is ready
 $(function() {
     
     var column_data = [ 
-        {"column_id": 1, "column_name": "HTML5 VI"}, 
-        {"column_id": 2, "column_name": "MAD"}, 
-        {"column_id": 3, "column_name": "Research"},
-        {"column_id": 4, "column_name": "Others"}
+        {"column_id": "class1", "column_name": "HTML5 VI"}, 
+        {"column_id": "class2", "column_name": "MAD"}, 
+        {"column_id": "class3", "column_name": "Research"},
+        {"column_id": "class4", "column_name": "Others"}
     ];
     
     var portlets_demo_data = [
@@ -32,14 +32,22 @@ $(function() {
         {"portlet_name": "Task", "portlet_detail": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit"}
     ]
     
+    // For to-do
     $.each(column_data, function (i, item) {
         // Add columns and kanbans for to do
         $('.columns-to-do').append(getColumnHTML(item.column_id, item.column_name, portlets_demo_data));                
     });
     
+    $("div.column-header span.header").append($('<span/>', {class: 'ui-icon ui-icon-plus add-button'}));
+    $("div.column-header span.header span.ui-icon-plus").click(function () {
+        $("#editing-area").data("eventsender", $(this).parents("div.column-header").siblings("div.column").attr('class').split(' ')[1]).dialog( "open" );
+        $("#editing-area #project-name").val($(this).parents("span.h2").text());
+    });    
+    
     // For doing
-    $('.columns-doing').append(getColumnHTML(5, 'Doing', portlets_demo_data));
+    $('.columns-doing').append(getColumnHTML('class-doing', 'Doing', portlets_demo_data));
     // For done
-    $('.columns-done').append(getColumnHTML(6, 'Done', portlets_demo_data));
+    $('.columns-done').append(getColumnHTML('class-done', 'Done', portlets_demo_data));
+    $('.columns-done .portlet-content').hide();
     
 });
--- fontend/js/initSortable.js
+++ fontend/js/initSortable.js
@@ -6,21 +6,21 @@ $(function() {
         cursor: "pointer",
         connectWith: ".column",
         beforeStop: function( event, ui ) {
-            ui.item.find(".portlet-header").removeClass().addClass("portlet-header").addClass(ui.item.parent().attr('class').split(' ')[1]);
+            if (ui.item.parent().attr('class').split(' ')[1] != 'class-doing') {
+                ui.item.find(".portlet-header").removeClass().addClass("portlet-header").addClass(ui.item.parent().attr('class').split(' ')[1]);
+            }
+            
+            if (ui.item.parent().attr('class').split(' ')[1] == 'class-done') {
+                ui.item.find(".portlet-content").hide();
+            } else if (!ui.item.find(".portlet-content").is(':visible')) {
+                ui.item.find(".portlet-content").show();
+            }
         }
     });
 
     $( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
       .find( ".portlet-header" )
-        .addClass( "ui-widget-header ui-corner-all" )
-        .prepend( "<span class='ui-icon ui-icon-minusthick'><\/span>")
-        .end()
-      .find( ".portlet-content" );
-
-    $( ".portlet-header .ui-icon" ).click(function() {
-      $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
-      $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
-    });
+        .addClass( "ui-widget-header ui-corner-all" );
 
     $( ".column" ).disableSelection();
 
